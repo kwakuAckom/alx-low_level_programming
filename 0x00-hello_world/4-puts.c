2f74759ffd96ec03aca8d851fc9821a30d17cc7a
kwakuAckom <84135615+kwakuAckom@users.noreply.github.com>
2023-06-15 20:32:08 +0000

    updated file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
-intmain(void)
+#include <stdio.h>
+
+/**
+* main - prints the phrase "Programming is like building a multilingual puzzle
+* Return: 0 if exited properly, non-zero otherwise
+*/
+
+int main(void)
 {
-puts("\"Programmingislikebuildingamultillingualpuzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
